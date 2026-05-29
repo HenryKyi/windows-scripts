7046526fd63498f6315891d161113fd2b311ecad
HenryKyi <153852495+HenryKyi@users.noreply.github.com>
2026-05-29 15:52:29 -0700

    Update gs5_createprofiles.ps1

--- gs5_createprofiles.ps1
+++ gs5_createprofiles.ps1
@@ -1,22 +1,16 @@
 # Clear the screen for a clean look
 Clear-Host
 
-Write-Host "=========================================" 
-Write-Host "      OOBE CLOUD DEPLOYMENT INITIALIZED   "
-Write-Host "========================================="
-Write-Host ""
+New-LocalUser -Name "GS5 Training" -NoPassword
+Add-LocalGroupMember -Group "Users" -Member "GS5 Training" -ErrorAction SilentlyContinue
 
-# 1. Test Network Connection
-Write-Host "[*] Verifying internet access..." 
-$ping = Test-Connection -ComputerName 8.8.8.8 -Count 2 -Quiet
-if ($ping) {
-    Write-Host "[+] Internet connection: ONLINE" 
-    ping 8.8.8.8
-} else {
-    Write-Host "[-] Internet connection: OFFLINE. Check your network adapter." 
-}
+$adminPwd = ConvertTo-SecureString "sUDHccPPmQGiZGxUEc6m" -AsPlainText -Force
+New-LocalUser -Name "gs5admin" -Password $adminPwd
+Add-LocalGroupMember -Group "Administrators" -Member "gs5admin"
 
-Write-Host ""
-# 2. Confirm Script Finished
-Write-Host "[+] Cloud execution pipeline test successful!"
-Write-Host "=========================================" 
+$sysAdminPwd = ConvertTo-SecureString "P@ssw0rd11545" -AsPlainText -Force
+Set-LocalUser -Name "Administrator" -Password $sysAdminPwd
+Enable-LocalUser -Name "Administrator"
+
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE" -Name "BypassNRO" -Value 1 -Force
+cmd.exe /c "oobe\bypassnro.cmd"
