5ccc87410286a62207025472268db3b05f5dd547
HenryKyi <153852495+HenryKyi@users.noreply.github.com>
2026-05-29 16:01:50 -0700

    Update gs5_createprofiles.ps1

--- gs5_createprofiles.ps1
+++ gs5_createprofiles.ps1
@@ -12,5 +12,5 @@ $sysAdminPwd = ConvertTo-SecureString "P@ssw0rd11545" -AsPlainText -Force
 Set-LocalUser -Name "Administrator" -Password $sysAdminPwd
 Enable-LocalUser -Name "Administrator"
 
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE" -Name "BypassNRO" -Value 1 -Force
-& "$env:SystemRoot\System32\oobe\bypassnro.cmd"
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE" /v BypassNRO /t REG_DWORD /d 1 /f
+shutdown /r /t 0
